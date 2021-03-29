092a38b3f8a0d74396fa01fa17fd49d752f69b59
rrafalia <rajrafalia@gmail.com>
2021-03-29 17:48:49 -0400

    Added get_player_xy. Fixed some functions regarding radius .

--- world_manager.mli
+++ world_manager.mli
@@ -3,3 +3,9 @@
    the calculated "local" entities and predicts where such entities will be*)
 val get_local :
   Common.world_state -> float -> float -> Common.entity list
+
+
+(*[get_player_xy] returns a player id if World_state has a uuid. If such a uuid 
+   exists search world_state.data for the uuid and return (x * y) is exists else 
+   None*)
+val get_player_xy : Common.world_state -> (int * int ) option
